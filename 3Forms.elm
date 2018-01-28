6fa30409acf41121c9c9ed047c45b36328d76fc8
Unknown <steven.smith458@gmail.com>
2018-01-28 12:21:45 -0500

    Added check for password length

--- 3Forms.elm
+++ 3Forms.elm
@@ -1,17 +1,21 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
 
+
 -- Model
 
+
 type alias Model =
-    { name: String
-    , password: String
+    { name : String
+    , password : String
     , passwordAgain : String
     }
 
@@ -21,12 +25,15 @@ model =
     Model "" "" ""
 
 
+
 -- Update
+
 
 type Msg
     = Name String
     | Password String
     | PasswordAgain String
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -41,24 +48,29 @@ update msg model =
             { model | passwordAgain = password }
 
 
+
 -- View
+
 
 view : Model -> Html Msg
 view model =
     div []
-      [ input [ type_ "text", placeholder "Name", onInput Name ] []
-      , input [ type_ "password", placeholder "Password", onInput Password ] []
-      , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain] []
-      , viewValidation model
-      ]
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , viewValidation model
+        ]
+
 
 viewValidation : Model -> Html msg
 viewValidation model =
     let
-        (color, message) =
-          if model.password == model.passwordAgain then
-            ("green", "OK")
-          else
-            ("red", "Passwords do not match!")
+        ( color, message ) =
+            if model.password /= model.passwordAgain then
+                ( "red", "Passwords do not match!" )
+            else if String.length model.password < 5 then
+                ( "red", "Password is too short!" )
+            else
+                ( "green", "OK" )
     in
-      div [ style [("color", color)] ] [ text message ]
+    div [ style [ ( "color", color ) ] ] [ text message ]
